95e7ecae5b519173d21239d09f51861e577cac13
outlander <outlander@192>
2022-11-13 21:05:27 -0500

    publish

---
+++ tcp-communication/TCP-server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import socket
+
+serverSocketObject = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM
+)
+
+host = '192.168.0.4' # replace with ur local ip (ifconfig) or (hostname)
+port = 444
+
+serverSocketObject.bind((host, port))
+
+
+# -TCP listener
+
+serverSocketObject.listen(3)
+
+while True:
+    clientsocket, address = serverSocketObject.accept()
+
+    print("Received connection from %r" % str(address))
+
+    message = 'Connected to %r' % str(host) + '!!'
+    clientsocket.send(message.encode('ascii'))
+
+    clientsocket.close()
